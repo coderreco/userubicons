c1fb43d7702a5c151ed6f546a30feee0cc99621e
coderreco <dev@coderre.co>
2024-01-11 22:06:58 -0800

    Fix typo in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 const Hero = () => (
   <div className="flex flex-col items-center justify-center mx-auto p-16"> 
     <h1 className="sr-only">Rubicons</h1>
-    <div className="text-5xl font-bold">Sharp icons for <span className='bg-gradient-to-r from-primary to-[#f50049] bg-clip-text text-transparent'>Sharp UI's.</span></div>
+    <div className="text-5xl font-bold">Sharp icons for <span className="bg-gradient-to-r from-primary to-[#f50049] bg-clip-text text-transparent">Sharp UI's.</span></div>
     <p className="text-lg text-center mt-4 max-w-2xl colr-slate-400">Pixel perfect, designed on a 24x24 pixel grid. Choose your color, download the full pack or single icons in SVG format, or quickly copy the SVG code.</p>
   </div>
 )
