e02969f95b5d5923c782de786cc6bcf9e7e4c046
coderreco <dev@coderre.co>
2024-01-11 22:04:42 -0800

    Remove unused code for search functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,7 @@
-import Image from 'next/image'
-import Search from './ui/search'
 import { IconFeed } from './ui/icons'
 
-const Icons = [
-  {
-    name: 'Search',
-    stroke: <svg width="auto" height="auto" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M8 21H5C3.34315 21 2 19.6569 2 18V6C2 4.34315 3.34315 3 5 3H19C20.6569 3 22 4.34315 22 6V9" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M2 11L8 11" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
-<circle cx="15.5" cy="15.5" r="4.5" stroke="currentColor" stroke-width="2"/>
-<path d="M22 22L19 19" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
-    </svg>,
-    solid: <svg width="auto" height="auto" viewBox="0 0 240 240" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M8 21H5C3.34315 21 2 19.6569 2 18V6C2 4.34315 3.34315 3 5 3H19C20.6569 3 22 4.34315 22 6V9" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-      <path d="M2 11L8 11" stroke="black" stroke-width="2" stroke-linecap="round" />
-      <circle cx="15.5" cy="15.5" r="4.5" stroke="black" stroke-width="2" />
-      <path d="M22 22L19 19" stroke="black" stroke-width="2" stroke-linecap="round" />
-    </svg>
-  }
-]
+export default function Home() {
 
-// Create type for icon schema
-
-
-export default function Home({ searchParams }: { searchParams: any }) {
-  // console.log(searchParams)
-  // const activeColor = searchParams.color || '#f7ded7'
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <Hero />
